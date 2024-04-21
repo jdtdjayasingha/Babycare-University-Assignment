9918dc1fdbf085dce38b0e56db514cd40d40923a
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-04-21 23:11:41 +0530

    Created the Doctor Dashboard Bottom Navigation Bar

---
+++ lib/screen/doctor_dashboard.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class DoctorDashboard extends StatefulWidget {
+  const DoctorDashboard({super.key});
+
+  @override
+  State<DoctorDashboard> createState() => _DoctorDashboardState();
+}
+
+class _DoctorDashboardState extends State<DoctorDashboard> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Schedule',
+      style: optionStyle,
+    ),
+    Text(
+      'Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined),
+            label: 'Schedule',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 31, 171, 137),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/screen/test.dart
+++ lib/screen/test.dart
@@ -1,5 +1,4 @@
-
-import 'package:babycare/screen/enter_doctor_details.dart';
+import 'package:babycare/screen/doctor_dashboard.dart';
 import 'package:flutter/material.dart';
 
 class Test extends StatelessWidget {
@@ -19,7 +18,7 @@ class Test extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => EnterDoctorDetails(),
+                      builder: (context) => DoctorDashboard(),
                     ),
                   );
                 },
