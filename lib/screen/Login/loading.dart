a8e7028f552cb1ef5fbef8efcf3ee7d67b78f54a
ggkWasundara <159466047+mokshamadusanka@users.noreply.github.com>
2024-04-29 15:54:46 +0530

    Created the Loading Screen

---
+++ lib/screen/Login/loading.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class LodingScreen extends StatefulWidget {
+  const LodingScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LodingScreenState createState() => _LodingScreenState();
+}
+
+class _LodingScreenState extends State<LodingScreen> {
+  double _progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startLoading();
+  }
+
+  void _startLoading() {
+    Timer.periodic(const Duration(milliseconds: 300), (Timer timer) {
+      if (_progress >= 1) {
+        timer.cancel();
+        _navigateToHome();
+      } else {
+        setState(() {
+          _progress += 0.1;
+        });
+      }
+    });
+  }
+
+  _navigateToHome() async {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const Scaffold(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(225, 0, 255, 171),
+              Color.fromARGB(225, 31, 171, 137),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 300,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 100,
+                  ),
+                  child: Image.asset(
+                    'assets/images/babycare.png',
+                    width: 270,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 80,
+                    right: 80,
+                  ),
+                  child: LinearProgressIndicator(
+                    value: _progress,
+                    backgroundColor: Color.fromARGB(180, 217, 217, 217),
+                    minHeight: 12.0,
+                    borderRadius: BorderRadius.circular(30),
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      const Color.fromARGB(200, 0, 128, 0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
