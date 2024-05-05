ebe780b5433e746a4fdf80bcd2a0dd68b25bef8c
Tharindu Dilshan <159466047+mokshamadusanka@users.noreply.github.com>
2024-05-05 13:57:54 +0530

    Created the User Sign In Screen

---
+++ lib/screen/Login/user/user_sign_in.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+class UserSignIn extends StatefulWidget {
+  const UserSignIn({super.key});
+
+  @override
+  State<UserSignIn> createState() => _UserSignInState();
+}
+
+class _UserSignInState extends State<UserSignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(
+            25.0,
+          ),
+          child: Column(
+            children: [
+              _header(context),
+              _inputField(context),
+              _signup(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return const Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "WELCOME BACK!",
+          style: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Enter Your Email",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none),
+              fillColor: Colors.green[50],
+              filled: true,
+              prefixIcon: const Icon(Icons.person)),
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Enter Your Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none),
+            fillColor: Colors.green[50],
+            filled: true,
+            prefixIcon: const Icon(Icons.lock),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Checkbox(
+                  side: MaterialStateBorderSide.resolveWith(
+                    (states) => const BorderSide(
+                      width: 1.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  value: false,
+                  onChanged: (newValue) {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7)),
+                ),
+                const Text(
+                  "Remember me",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Forget Password?",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Color.fromARGB(255, 31, 171, 137),
+            ),
+            child: const Center(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+      ],
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account?"),
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SignUpSelectRole()),
+              );
+            },
+            child: const Text(
+              "Sign Up",
+              style: TextStyle(color: Colors.green),
+            ))
+      ],
+    );
+  }
+}
