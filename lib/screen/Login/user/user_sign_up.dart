453845dd34a203e6b62ba9178e32005719176dd3
Tharindu Dilshan <159466047+mokshamadusanka@users.noreply.github.com>
2024-05-05 14:08:30 +0530

    Created User Sign Up Screen

---
+++ lib/screen/Login/user/user_sign_up.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class UserSignUp extends StatefulWidget {
+  const UserSignUp({super.key});
+
+  @override
+  State<UserSignUp> createState() => _UserSignUpState();
+}
+
+class _UserSignUpState extends State<UserSignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 25,
+            right: 25,
+            top: 5,
+          ),
+          child: Column(
+            children: [
+              _header(context),
+              _inputField(context),
+              _signup(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "CREATE USER",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "ACCOUT",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "User Email",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                borderSide: BorderSide.none),
+            fillColor: Colors.green[50],
+            filled: true,
+          ),
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none),
+            fillColor: Colors.green[50],
+            filled: true,
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(
+          height: 25,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Confirm Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none),
+            fillColor: Colors.green[50],
+            filled: true,
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Color.fromARGB(255, 31, 171, 137),
+            ),
+            child: const Center(
+              child: Text(
+                "Next",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+      ],
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Already have an account?"),
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const UserSignIn()),
+              );
+            },
+            child: const Text(
+              "Sign In",
+              style: TextStyle(color: Colors.green),
+            ))
+      ],
+    );
+  }
+}
