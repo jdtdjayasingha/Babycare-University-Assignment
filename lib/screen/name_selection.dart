07a94a3b40e05c901c7ae01fd369c99d36c99632
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-06-10 23:45:05 +0530

    Update name_selection.dart

--- lib/screen/name_selection.dart
+++ lib/screen/name_selection.dart
@@ -1,79 +1,79 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class NameSelection extends StatefulWidget {
-  const NameSelection({super.key});
+// class NameSelection extends StatefulWidget {
+//   const NameSelection({super.key});
 
-  @override
-  State<NameSelection> createState() => _NameSelectionState();
-}
+//   @override
+//   State<NameSelection> createState() => _NameSelectionState();
+// }
 
-class _NameSelectionState extends State<NameSelection> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 50,
-          right: 50,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 45,
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 31, 171, 137),
-                ),
-                onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => const AdminProfile()),
-                  // );
-                },
-                child: const Text(
-                  'Boy',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              height: 45,
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 31, 171, 137),
-                ),
-                onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => const UserProfile()),
-                  // );
-                },
-                child: const Text(
-                  'Girl',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class _NameSelectionState extends State<NameSelection> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Padding(
+//         padding: const EdgeInsets.only(
+//           left: 50,
+//           right: 50,
+//         ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             SizedBox(
+//               height: 45,
+//               width: double.infinity,
+//               child: ElevatedButton(
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Color.fromARGB(255, 31, 171, 137),
+//                 ),
+//                 onPressed: () {
+//                   // Navigator.push(
+//                   //   context,
+//                   //   MaterialPageRoute(
+//                   //       builder: (context) => const AdminProfile()),
+//                   // );
+//                 },
+//                 child: const Text(
+//                   'Boy',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(
+//               height: 25,
+//             ),
+//             SizedBox(
+//               height: 45,
+//               width: double.infinity,
+//               child: ElevatedButton(
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Color.fromARGB(255, 31, 171, 137),
+//                 ),
+//                 onPressed: () {
+//                   // Navigator.push(
+//                   //   context,
+//                   //   MaterialPageRoute(
+//                   //       builder: (context) => const UserProfile()),
+//                   // );
+//                 },
+//                 child: const Text(
+//                   'Girl',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
