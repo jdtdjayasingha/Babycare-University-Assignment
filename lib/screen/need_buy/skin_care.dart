742b4d32d43e43350162e2da3ad50e7883de5b54
Tharindu Dilshan <109893959+Dilmi-sathsarani@users.noreply.github.com>
2024-06-12 16:33:21 +0530

    Create skin_care.dart

---
+++ lib/screen/need_buy/skin_care.dart
@@ -0,0 +1,375 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/screen/need_buy/maternity_wear.dart';
+import 'package:mobile_app/screen/need_buy/pain_relief.dart';
+
+class SkinCare extends StatelessWidget {
+  const SkinCare({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        toolbarHeight: 62,
+        backgroundColor: Colors.white,
+        shadowColor: const Color.fromARGB(255, 255, 255, 255),
+        automaticallyImplyLeading: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(
+                    Icons.menu_rounded,
+                    size: 35,
+                  ),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  tooltip:
+                      MaterialLocalizations.of(context).openAppDrawerTooltip,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        width: 150,
+        elevation: 0,
+        shape: const BeveledRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        backgroundColor: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 50,
+          ),
+          child: ListView(
+            children: [
+              ListTile(
+                title: Padding(
+                  padding: EdgeInsets.only(
+                    left: 15,
+                  ),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 48,
+                        backgroundImage:
+                            AssetImage('assets/images/maternitymain.png'),
+                      ),
+                      Text(
+                        "Maternity",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "Wear",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MaternityWear()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Padding(
+                  padding: EdgeInsets.only(
+                    left: 15,
+                  ),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 48,
+                        backgroundImage:
+                            AssetImage('assets/images/SkinCare.jpg'),
+                      ),
+                      Text(
+                        'Skin Care',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SkinCare()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Padding(
+                  padding: EdgeInsets.only(
+                    left: 15,
+                  ),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 48,
+                        backgroundImage:
+                            AssetImage('assets/images/painmain.png'),
+                      ),
+                      Text(
+                        'Pain',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Relief',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PainRelief()),
+                  );
+                },
+              ),
+              // ListTile(
+              //   title: Padding(
+              //     padding: EdgeInsets.only(
+              //       left: 15,
+              //     ),
+              //     child: Column(
+              //       children: [
+              //         Image.asset(
+              //           'assets/images/wear.png',
+              //           width: 180,
+              //         ),
+              //         Text(
+              //           'Daily',
+              //           style: TextStyle(
+              //             color: Colors.grey,
+              //             fontSize: 19,
+              //             fontWeight: FontWeight.w600,
+              //           ),
+              //         ),
+              //         Text(
+              //           'Wellness',
+              //           style: TextStyle(
+              //             color: Colors.grey,
+              //             fontSize: 19,
+              //             fontWeight: FontWeight.w600,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              //   onTap: () {},
+              // ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: Text(
+                  "Skin Care",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 30,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      height: 150,
+                                      width: 150,
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(25)),
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                            "assets/images/Cerave.jpg",
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  bottom: 10,
+                                  top: 10,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Cerave",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w700),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      height: 150,
+                                      width: 150,
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(25)),
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                            "assets/images/Laroche Posay.jpg",
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  bottom: 10,
+                                  top: 10,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Laroche Posay",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w700),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 40,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Column(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        height: 150,
+                                        width: 150,
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(25)),
+                                          image: DecorationImage(
+                                            image: AssetImage(
+                                              "assets/images/The Ordinary.webp",
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(
+                                    bottom: 10,
+                                    top: 10,
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      "The Ordinary",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
