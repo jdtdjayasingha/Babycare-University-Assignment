b9e31c3c79ee48f5fc47c3469941cc6983db0ee6
Tharindu Dilshan <109893959+Dilmi-sathsarani@users.noreply.github.com>
2024-06-12 16:35:01 +0530

    Update skin_care.dart

--- lib/screen/need_buy/skin_care.dart
+++ lib/screen/need_buy/skin_care.dart
@@ -1,6 +1,6 @@
+import 'package:babycare/screen/need_buy/maternity_wear.dart';
+import 'package:babycare/screen/need_buy/pain_relief.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_app/screen/need_buy/maternity_wear.dart';
-import 'package:mobile_app/screen/need_buy/pain_relief.dart';
 
 class SkinCare extends StatelessWidget {
   const SkinCare({super.key});
@@ -154,38 +154,6 @@ class SkinCare extends StatelessWidget {
                   );
                 },
               ),
-              // ListTile(
-              //   title: Padding(
-              //     padding: EdgeInsets.only(
-              //       left: 15,
-              //     ),
-              //     child: Column(
-              //       children: [
-              //         Image.asset(
-              //           'assets/images/wear.png',
-              //           width: 180,
-              //         ),
-              //         Text(
-              //           'Daily',
-              //           style: TextStyle(
-              //             color: Colors.grey,
-              //             fontSize: 19,
-              //             fontWeight: FontWeight.w600,
-              //           ),
-              //         ),
-              //         Text(
-              //           'Wellness',
-              //           style: TextStyle(
-              //             color: Colors.grey,
-              //             fontSize: 19,
-              //             fontWeight: FontWeight.w600,
-              //           ),
-              //         ),
-              //       ],
-              //     ),
-              //   ),
-              //   onTap: () {},
-              // ),
             ],
           ),
         ),
