da84a3bd4c474f61613e43c2e8a7365bb710ee61
Tharindu Dilshan <159459640+Hasithms@users.noreply.github.com>
2024-06-18 02:14:54 +0530

    Create screen7.dart

---
+++ lib/screen/exerciseandfitness/screen7.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: use_key_in_widget_constructors, camel_case_types
+
+import 'package:flutter/material.dart';
+
+class screen7 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 42, 233, 100),
+      actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
+            ],
+      ),
+      body:Padding(padding: const EdgeInsets.all(16),
+      child:SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+
+
+
+          ],
+        ),
+      )
+        
+      ),
+    );
+  }
+}
